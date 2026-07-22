03e035a73ff36941cab0989a89ba46e6b090e5c4
zhaoxuexiao <32368325@163.com>
2026-07-22 17:33:00 +0800

    fix: 调用initDB云函数创建集合

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,22 +2,18 @@ App({
   onLaunch: function() {
     if (wx.cloud) {
       wx.cloud.init({ env: 'cloudbase-d1gq1v8xl0c056a2a', traceUser: true })
-      // Create collections on first launch
-      this._initCloudDB()
+      // Call cloud function to init DB
+      var created = wx.getStorageSync('_cloud_cols_created')
+      if (!created) {
+        wx.cloud.callFunction({
+          name: 'initDB',
+          success: function(res) {
+            console.log('initDB result:', res)
+            wx.setStorageSync('_cloud_cols_created', true)
+          },
+          fail: function(err) { console.log('initDB error:', err) }
+        })
+      }
     }
-  },
-  _initCloudDB: function() {
-    var db = wx.cloud.database()
-    var cols = ['posts', 'comments', 'likes', 'atlas']
-    var created = wx.getStorageSync('_cloud_cols_created')
-    if (created) return
-    cols.forEach(function(name) {
-      db.createCollection(name).then(function() {
-        console.log('Collection created: ' + name)
-      }).catch(function(e) {
-        console.log('Collection ' + name + ': ' + (e.errCode === -1 ? 'exists' : e.message))
-      })
-    })
-    wx.setStorageSync('_cloud_cols_created', true)
   }
 })
