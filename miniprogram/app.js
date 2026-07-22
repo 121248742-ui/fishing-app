b205c1c0b54d1a74bc2d3ea0df9413b654f314e5
zhaoxuexiao <32368325@163.com>
2026-07-22 17:47:40 +0800

    fix: 一次性清除所有本地数据

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,18 +2,14 @@ App({
   onLaunch: function() {
     if (wx.cloud) {
       wx.cloud.init({ env: 'cloudbase-d1gq1v8xl0c056a2a', traceUser: true })
-      // Call cloud function to init DB
-      var created = wx.getStorageSync('_cloud_cols_created')
-      if (!created) {
-        wx.cloud.callFunction({
-          name: 'initDB',
-          success: function(res) {
-            console.log('initDB result:', res)
-            wx.setStorageSync('_cloud_cols_created', true)
-          },
-          fail: function(err) { console.log('initDB error:', err) }
-        })
-      }
+      // Clear all local posts (one-time cleanup)
+      wx.removeStorageSync('circle_posts')
+      wx.removeStorageSync('fish_atlas')
+      wx.removeStorageSync('weather_spots')
+      wx.removeStorageSync('weather_active_spot')
+      wx.removeStorageSync('weather_fish_idx')
+      wx.removeStorageSync('userInfo')
+      wx.removeStorageSync('_cloud_cols_created')
     }
   }
 })
