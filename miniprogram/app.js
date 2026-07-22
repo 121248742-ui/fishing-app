ab020c1c6b10c65f4da3f215ab152dce29039f2a
zhaoxuexiao <32368325@163.com>
2026-07-22 17:51:32 +0800

    fix: 恢复正常app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,14 +2,6 @@ App({
   onLaunch: function() {
     if (wx.cloud) {
       wx.cloud.init({ env: 'cloudbase-d1gq1v8xl0c056a2a', traceUser: true })
-      // Clear all local posts (one-time cleanup)
-      wx.removeStorageSync('circle_posts')
-      wx.removeStorageSync('fish_atlas')
-      wx.removeStorageSync('weather_spots')
-      wx.removeStorageSync('weather_active_spot')
-      wx.removeStorageSync('weather_fish_idx')
-      wx.removeStorageSync('userInfo')
-      wx.removeStorageSync('_cloud_cols_created')
     }
   }
 })
