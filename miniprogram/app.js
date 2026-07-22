8fafcf9b4d03430457ba531c6c2c424f77040fdc
zhaoxuexiao <32368325@163.com>
2026-07-22 17:17:48 +0800

    fix: 更新云环境ID

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,7 @@ App({
   onLaunch: function() {
     // Init cloud
     if (wx.cloud) {
-      wx.cloud.init({ env: 'cloud1-5g7p2gkseb26e491', traceUser: true })
+      wx.cloud.init({ env: 'cloudbase-d1gq1v8xl0c056a2a', traceUser: true })
     }
   }
 })
